75387120bfedbd97a30bd2f5996ae53432840df2
Taras Garan'ko <garanko.t@gmail.com>
2022-07-15 23:46:16 +0300

    Update products.js

--- routes/api/products.js
+++ routes/api/products.js
@@ -5,6 +5,6 @@ const { searchProducts } = require("../../controllers/products");
 const { dailyRateNotAllowProducts } = require("../../controllers/dailyRate");
 
 router.get("/search/:query", searchProducts);
-router.get("/public/:bloodType", dailyRateNotAllowProducts);
+router.post("/public/:bloodType", dailyRateNotAllowProducts);
 
 module.exports = router;
