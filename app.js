bf2fd3af7fd699c7cfedb02fb41b9885de26a729
Taras Garan'ko <garanko.t@gmail.com>
2022-07-20 00:16:02 +0300

    fix

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const dotenv = require("dotenv");
 dotenv.config();
 
 const authRouter = require("./routes/api/auth");
-const productsRouter=require('./routes/api/products');
-const diaryRouter=require('./routes/api/diary');
+const productsRouter = require("./routes/api/products");
+const diaryRouter = require("./routes/api/diary");
 const usersRauter = require("./routes/api/users");
 
 const app = express();
@@ -19,10 +21,11 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRouter);
-app.use('/api/products', productsRouter);
-app.use('/api/diary', diaryRouter)
+app.use("/api/products", productsRouter);
+app.use("/api/diary", diaryRouter);
 app.use("/api/users", usersRauter);
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res, next) => {
   next({ status: 404, message: "Not Found" });
